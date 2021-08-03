4a11941c05ae892b8d3e290209350cfd45fa58f7
obada-naser <obadanaser135@gmail.com>
2021-08-03 03:06:04 +0300

    trying to solve the problems

--- server.js
+++ server.js
@@ -1,57 +1,73 @@
 'use strict';
 
 
-const express =require('express');
+const express = require('express');
+const dotenv=require('dotenv');
+const server = express();
+const cors = require('cors');
+const weatherData = require('./data/weather.json');
+const {request}=require('express');
 
-const server=express();
-const cors=require('cors');
-const weatherData=require('./data/weather.json');
-const { setEnvironmentData } = require('worker_threads');
-const { send } = require('process');
-
-
-const PORT=3000;
+// const { setEnvironmentData } = require('worker_threads');
+// const { send } = require('process');
+dotenv.config();
+server.use(cors());
+const PORT = 3000;
 // process.env.PORT;
 
 
 
 
 
-server.get('/weather',(req,res)=>{
+server.get('/weather', (req, res) => {
   console.log(req.query);
-//   const lat=req.query.lat;
-//   const long=req.query.long;
-  const searchQuery=req.query.city_name;
- 
+    const lat=req.query.lat;
+    const long=req.query.long;
+  const searchQuery = req.query.searchQuery;
+
+  let findCity = weatherData.find((item)=>{
 
 
-  let findCity=weatherData.find(item=>{
-    if(item.city_name===searchQuery){
-      return [item.searchQuery,item.lat,item.long];
+  if (item.city_name == searchQuery || item.long == long || item.lat == lat) {
+    console.log(item.city_name);
 
+  }
+  else{
+    return'error 404 ';
+  }
+};
+
+
+  class Forcast {
+    constructor(date, description) {
+      this.date = date;
+      this.description = description;
     }
-    else{
-      return'error';
-    }
 
 
+  }
 
+  let newArr=[];
 
+  findCity.data.map((value,idx)=>{
+    newArr.push(new Forcast(value.datetime,`${value.low_temp},${value.high_temp},${value.weather.description}`));
   });
-  
+
+
+
   console.log(findCity);
   res.send(findCity);
 
 });
-server.get('*',(req,res)=>{
-    res.status('404').send('page not found');
-})
+server.get('*', (req, res) => {
+  res.status('404').send('page not found');
+});
 
 
 
 
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
   console.log(`I am listening on port ${PORT}`);
 
 });
