96829394cc3317727fbb7085a719b5c42af250e8
obada-naser <obadanaser135@gmail.com>
2021-08-06 16:07:25 +0300

    adding some changes

--- Forcast.js
+++ Forcast.js
@@ -4,12 +4,12 @@
 
 
 class Forcast {
-    constructor(value) {
-      this.date = value.valid_date;
-      this.description = value.weather.description;
-    }
-  
+  constructor(value) {
+    this.date = value.valid_date;
+    this.description = value.weather.description;
   }
+
+}
 
 module.exports = Forcast;
 
